3cb5afe2d822dbfc8a1b4248905d58b1e119f6ec
meebee70 <ckozbial321@gmail.com>
2016-11-14 14:46:40 -0700

    added gamepanel, and resized it to be appropriate

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -10,6 +10,7 @@ public class Game {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLayout(new BorderLayout());
+		frame.add(new GamePanel(), BorderLayout.CENTER);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
