96a6b4c31e0b26e57b0d3653c68b99a321d039e5
meebee70 <ckozbial321@gmail.com>
2016-11-14 15:04:06 -0700

    added thread

--- src/Game/GamePanel.java
+++ src/Game/GamePanel.java
@@ -1,18 +1,33 @@
 package Game;
 
 import java.awt.Dimension;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable, KeyListener {
 	private static final long serialVersionUID = 1l;
 	
 	public static final int WIDTH = 1000;
 	public static final int HEIGHT = 600;
 	
 	
+	private Thread thread;
+	private boolean isRunning = false;
+	
 	public GamePanel(){
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		
+		start();
+	}
+	
+	private void start(){
+		isRunning = true;
+		thread = new thread(this);
+		thread.start();
+	}
+	
+	public void run(){
 		
 	}
 
