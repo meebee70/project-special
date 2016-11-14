1b49d4d23c2ce4fdebe12abb9c32a513d2429896
Christopher Kozbial <ckozbial321@gmail.com>
2016-11-14 13:32:25 -0700

    Made a frame

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,5 +1,19 @@
 package Game;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 public class Game {
+	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLayout(new BorderLayout());
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
 
 }
