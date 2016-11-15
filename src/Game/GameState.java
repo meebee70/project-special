c5b5f4f79fb3cd68f4c130f3ecbbdb7e516eac05
meebee70 <ckozbial321@gmail.com>
2016-11-15 13:13:27 -0700

    added init() method

--- src/Game/GameState.java
+++ src/Game/GameState.java
@@ -2,6 +2,14 @@ package Game;
 
 public abstract class GameState {
 
+	protected GameStateManager gsm;
 	
+	protected GameState(GameStateManager gsm){
+		this.gsm = gsm;
+		init();
+	}
+	
+	public abstract void init();
+		
 	
 }
