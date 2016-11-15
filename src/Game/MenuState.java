f257a21b4069af72d424f3cd19ecb87986e095cf
meebee70 <ckozbial321@gmail.com>
2016-11-15 13:35:28 -0700

    added basic methods and extends GameState

--- src/Game/MenuState.java
+++ src/Game/MenuState.java
@@ -1,6 +1,44 @@
 package Game;
 
-public class MenuState {
+import java.awt.Graphics;
+
+public class MenuState extends GameState {
+	
+	public MenuState(GameStateManager gsm){
+		super(gsm);
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(int k) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 	
 
 }
