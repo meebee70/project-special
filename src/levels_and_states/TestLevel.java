33fdcb8256b2e56c82c1b5bf6c110eaa218e12f0
meebee70 <ckozbial321@gmail.com>
2016-11-15 14:49:54 -0700

    created new test level frame

---
+++ src/levels_and_states/TestLevel.java
@@ -0,0 +1,42 @@
+package levels_and_states;
+
+import java.awt.Graphics;
+
+public class TestLevel extends GameState{
+	
+	
+	public TestLevel(GameStateManager gsm){
+		super(gsm);
+	}
+
+
+	public void init() {
+
+		
+	}
+
+
+	public void tick() {
+
+		
+	}
+
+
+	public void draw(Graphics g) {
+
+		
+	}
+
+
+	public void keyPressed(int k) {
+
+		
+	}
+
+
+	public void keyReleased(int k) {
+
+		
+	}
+
+}
