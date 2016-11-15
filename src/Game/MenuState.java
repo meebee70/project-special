6488c6ed849be77cb47ad17c2884f2eee2f0b8ac
meebee70 <ckozbial321@gmail.com>
2016-11-15 13:36:23 -0700

    cleaned up the code

--- src/Game/MenuState.java
+++ src/Game/MenuState.java
@@ -8,33 +8,24 @@ public class MenuState extends GameState {
 		super(gsm);
 	}
 
-	@Override
+	
 	public void init() {
-		// TODO Auto-generated method stub
-		
+				
 	}
 
-	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void keyPressed(int k) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void keyReleased(int k) {
-		// TODO Auto-generated method stub
 		
 	}
 	
