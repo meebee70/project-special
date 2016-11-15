8c3f83aeafe4cdd19588144167987b8ace734b22
meebee70 <ckozbial321@gmail.com>
2016-11-15 13:02:13 -0700

    added state manager class and basic commands

---
+++ src/Game/GameStateManager.java
@@ -0,0 +1,25 @@
+package Game;
+
+import java.awt.Graphics;
+
+public class GameStateManager {
+	
+	public GameStateManager(){
+		
+	}
+
+	public void tick(){
+		
+	}
+	
+	public void draw(Graphics g){
+		
+	}
+	public void keyPressed(int k){
+		
+	}
+	
+	public void keyReleased(int k){
+		
+	}
+}
