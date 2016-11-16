60ad59a1417cc1980a5a01db3dc0ce1ba318003d
meebee70 <ckozbial321@gmail.com>
2016-11-16 14:33:26 -0700

    moved the main to it's own class

---
+++ src/mainGameEngine/Main.java
@@ -0,0 +1,14 @@
+package mainGameEngine;
+
+public class Main {
+	
+	static MainMenu menu = new MainMenu();
+	
+	
+	public static void main(String[] args){
+		
+		menu.run();
+		System.exit(0);
+	}
+
+}
