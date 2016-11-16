c201c91e25e003f177c1a7ebdad88fd617a1a9de
meebee70 <ckozbial321@gmail.com>
2016-11-16 13:34:42 -0700

    added Basic level Skeleton

--- src/levels/Level.java
+++ src/levels/Level.java
@@ -1,5 +1,21 @@
 package levels;
 
 public abstract class Level {
+	
+	private String name;
+	
+	public Level(String name){
+		this.name = name;
+	}
+	
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public abstract void initialize();
+	public abstract void update();
+	public abstract void draw();
+	
 
 }
