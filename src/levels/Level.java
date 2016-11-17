b8684f715c10bb19892839407bcc597bd8d97215
meebee70 <ckozbial321@gmail.com>
2016-11-17 14:18:20 -0700

    made generic changes to level skeleton

--- src/levels/Level.java
+++ src/levels/Level.java
@@ -2,10 +2,39 @@ package levels;
 
 public abstract class Level {
 	
+	
+	private boolean isRunning = true;
+	private final int fps = 60;
 	private String name;
 	
 	public Level(String name){
 		this.name = name;
+	}
+	
+	public void run(){
+		initialize();
+		
+		long time = System.currentTimeMillis();
+		
+		while (isRunning){
+			update();
+			draw();
+			
+			
+		//  delay for each frame  -   time it took for one frame 
+					time = (1000 / fps) - (System.currentTimeMillis() - time); 
+					
+					if (time > 0) 
+					{ 
+						try 
+						{ 
+							Thread.sleep(time); 
+						} 
+						catch(Exception e){} 
+					} 
+		}
+		
+		
 	}
 	
 	
@@ -21,6 +50,9 @@ public abstract class Level {
 	
 	//draws the level
 	public abstract void draw();
+	
+	
+	
 	
 	
 
--- src/mainGameEngine/Main.java
+++ src/mainGameEngine/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args){
 		
 		menu.run();
+		
 		System.exit(0);
 	}
-
 }
