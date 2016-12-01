4c06bdc534920f450ebda31c594a7c5576dace78
meebee70 <ckozbial321@gmail.com>
2016-12-01 14:08:05 -0700

    did stuff

--- src/characters/PlayerOne.java
+++ src/characters/PlayerOne.java
@@ -1,5 +1,39 @@
 package characters;
 
+import java.util.Objects;
+
 public class PlayerOne {
+	
+	private final int X_SPEED = 3; // pixels/frame
+	private int direction; //positive for true, negative for false
+	private double ySpeed = 0;
+	
+	private double x;
+	private double y;
+	
+	private String sprite;
+	
+	public PlayerOne(double x,double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setDirectiction(int direction){
+		this.direction = direction;
+	}
+	
+	public void affectDirection(int amount){
+		this.direction += amount;
+	}
+	
+	public void move(){
+		int xMove = X_SPEED + direction;
+		this.x = xMove;
+	}
+	
+	public void collide(Object[] objects){
+		System.out.println(Objects.class);
+	}
+	
 
 }
