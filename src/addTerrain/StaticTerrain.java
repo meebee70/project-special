19210017e67a5f00740a9c49401f3ad8d39925fc
meebee70 <ckozbial321@gmail.com>
2016-12-01 14:53:57 -0700

    added comments for understandability

--- src/addTerrain/StaticTerrain.java
+++ src/addTerrain/StaticTerrain.java
@@ -4,11 +4,27 @@ public class StaticTerrain extends Terrain {
 	private final int X;
 	private final int Y;
 	
-	
+	/*creates the terrain at the specified locations
+	 * 
+	 */
 	public StaticTerrain(int x,int y, int width, int height, String sprite){
 		super(width,height,sprite);
 		this.X = x;
 		this.Y = y;
+	}
+	
+	/*
+	 * @see addTerrain.Terrain#getX()
+	 */
+	public int getX(){
+		return X;
+	}
+	
+	/*
+	 * @see addTerrain.Terrain#getY()
+	 */
+	public int getY(){
+		return Y;
 	}
 
 }
--- src/addTerrain/Terrain.java
+++ src/addTerrain/Terrain.java
@@ -10,7 +10,9 @@ public abstract class Terrain {
 	private final String sprite;
 	
 	
-	
+	/*generic constructor
+	 * 
+	 */
 	public Terrain(int x, int y, int width, int height,String sprite){
 		this.x = x;
 		this.y = y;
@@ -19,28 +21,46 @@ public abstract class Terrain {
 		this.sprite = sprite;
 	}
 	
+	/*Constructor used for non-moving (static) terrain
+	 * 
+	 */
 	public Terrain(int width, int height, String sprite){
 		this.WIDTH = width;
 		this.HEIGHT = height;
 		this.sprite = sprite;
 	}
 	
+	/*gets the left most x position of this terrain
+	 * 
+	 */
 	public int getX(){
 		return x;
 	}
 	
+	/*gets the top most y position of this terrain
+	 * 
+	 */
 	public int getY(){
 		return y;
 	}
 	
+	/*gets the width of this terrain (from the left)
+	 * 
+	 */
 	public int getWidth(){
 		return WIDTH;
 	}
 	
+	/*gets the height of this terrain (from the top)
+	 * 
+	 */
 	public int getHeight(){
 		return HEIGHT;
 	}
 	
+	/*gets the address (as a string) of the sprite
+	 * 
+	 */
 	public String getSprite(){
 		return this.sprite;
 	}
