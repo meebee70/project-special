e9035d5e7a1dd7341c4c1a59399639dcba1e25ce
meebee70 <ckozbial321@gmail.com>
2016-12-01 14:55:54 -0700

    added mobile terrain class

---
+++ src/addTerrain/MobileTerrain.java
@@ -0,0 +1,10 @@
+package addTerrain;
+
+public class MobileTerrain extends Terrain{
+	
+	
+	public MobileTerrain(int x, int y, int width, int height, String sprite){
+		super(x,y,width,height,sprite);
+	}
+
+}
