a417dc6efcfb284dc2e7caa2ed6696341b708a4f
meebee70 <ckozbial321@gmail.com>
2016-12-06 13:59:00 -0700

    made physics class

---
+++ src/physics/Physics.java
@@ -0,0 +1,47 @@
+package physics;
+
+public class Physics {
+	private final double defaultGravity = 0.1;
+	private final double defaultFriction = 0.08;
+	
+	public double gravity;
+	public double friction;
+	
+
+	public Physics(){
+		this.gravity = defaultGravity;
+		this.friction = defaultFriction;
+	}
+	
+	public Physics(double gravity){
+		this.gravity = gravity;
+		this.friction = defaultFriction;
+	}
+	
+	public Physics(double gravity, double friction){
+		this.gravity = gravity;
+		this.friction = friction;
+	}
+	
+	//Accessors
+	public double getGravity(){
+		return this.gravity;
+	}
+	
+	public double getFriction(){
+		return this.friction;
+	}
+	
+	//Mutators
+	public void setGravity(double amount){
+		this.gravity = amount;
+	}
+	
+	public void setFriction(double amount){
+		this.friction = amount;
+	}
+
+	
+	
+
+}
