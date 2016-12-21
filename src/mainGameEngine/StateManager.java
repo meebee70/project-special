a83415a7dcf52324e127e76be937164450dbac15
Christopher Kozbial <ckozbial321@gmail.com>
2016-12-21 15:05:27 -0700

    added getters and an icon

--- src/mainGameEngine/StateManager.java
+++ src/mainGameEngine/StateManager.java
@@ -1,9 +1,12 @@
 package mainGameEngine;
 
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Stack;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 import levels.Level;
@@ -24,14 +27,22 @@ public class StateManager extends JFrame {
 	public InputHandler input;
 	public Physics physics;
 	
+	private Image img;
+	
 	public Stack<Level> levels = new Stack<Level>();
 	
 	public StateManager(){
-		setTitle("Game Tutorial"); 
+		setTitle("Project Helios"); 
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT); 
 		setResizable(false); 
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 
-		setVisible(true);
+		
+		try{
+			img = ImageIO.read(new File("res/Laughing Stock.gif"));
+		}catch (Exception e){}
+		setIconImage(img);
+		
+		this.setVisible(true);
 		
 		
 		insets = getInsets(); 
@@ -41,5 +52,42 @@ public class StateManager extends JFrame {
 		backBuffer = new BufferedImage(UNIVERSE_WIDTH, UNIVERSE_HEIGHT, BufferedImage.TYPE_INT_RGB); 
 		input = new InputHandler(this);
 		physics = new Physics();
+	}
+
+	
+	public int getWindowHeight(){
+		return this.WINDOW_HEIGHT;
+	}
+	
+	public int getWindowWidth(){
+		return this.WINDOW_WIDTH;
+	}
+	
+	public int getUniverseHeight(){
+		return this.UNIVERSE_HEIGHT;
+	}
+	
+	public int getUniverseWidth(){
+		return this.UNIVERSE_WIDTH;
+	}
+	
+	public Insets getOurInsets(){
+		return this.insets;
+	}
+	
+	public BufferedImage getUniverse(){
+		return this.backBuffer;
+	}
+	
+	public InputHandler getInputHandler(){
+		return this.input;
+	}
+	
+	public Physics getPhysics(){
+		return this.physics;
+	}
+	
+	public Stack<Level> getLevels(){
+		return this.levels;
 	}
 }
