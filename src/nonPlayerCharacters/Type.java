32f1d31905194b0b180abd0b4fc36632d62326b8
meebee70 <ckozbial321@gmail.com>
2017-01-10 14:31:14 -0700

    added various tags to the enemies to describe their specialties

--- src/nonPlayerCharacters/Type.java
+++ src/nonPlayerCharacters/Type.java
@@ -7,14 +7,14 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public enum Type {
-	GOOMBA("res/Laughing Stock.gif"),
-	FLYER("res/Tainted Ground v1.png");
+	GOOMBA("res/Laughing Stock.gif", new Tags[]{Tags.UNSPECIAL}),
+	FLYER("res/Tainted Ground v1.png", new Tags[]{Tags.FLYING});
 	//add more as we need them, also change these as needed
 	
-	
+	private Tags[] tags;
 	private Image img;
 	
-	Type(String image){
+	Type(String image, Tags[] tags){
 		
 		try{
 			img = ImageIO.read(new File(image));
@@ -22,6 +22,7 @@ public enum Type {
 			System.err.println(image + " is not a valid image location");
 		}
 		
+		this.tags = tags;
 	}
 	
 	public Image getImage(){
